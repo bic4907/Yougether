ba2aba965815fed45d28f1d37f6c3a0d71b7eb1f
minchan <ahwere@naver.com>
2019-11-11 17:45:35 +0900

    Room Controller

---
+++ app/Http/Controllers/Room/CreateController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Room;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CreateController extends Controller
+{
+    public function makingRoom(Request $request)
+    {
+        dd($request);
+    }
+}
