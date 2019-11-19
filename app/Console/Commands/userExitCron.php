c88a39268174094ed31ef28ef09e49c1a3d433e9
minchan <ahwere@naver.com>
2019-11-19 17:25:13 +0900

    checking user exit browser

---
+++ app/Console/Commands/userExitCron.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class userExitCron extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:exit';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'checking exit for browser';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        User::where('last_hit', '<', Carbon::now()->addSeconds(-3))->update(['room_id'=>Null]);
+    }
+}
--- daemon.sh
+++ daemon.sh
@@ -2,5 +2,7 @@
 
 while true; do
 	php artisan room:manage
+	php artisan user:delete
+	php artisan user:exit
 	sleep 1
 done
