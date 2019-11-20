44ff92d90371a275a4bc7a59f4d21bc19bf6c353
eogh234 <eogh234@naver.com>
2019-11-20 21:05:35 +0900

    UserLog table migration, model 추가

---
+++ database/migrations/2019_11_20_115302_create_user_logs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_logs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedInteger('room_id');
+            $table->foreign('room_id')->references('id')->on('rooms')->onDelete('cascade');;
+            $table->boolean('rewind');
+            $table->boolean('add_video');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_logs');
+    }
+}
