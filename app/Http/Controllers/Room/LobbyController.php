d1c26705d89407038f467fa3e6e96aba50545358
bic4907 <bic4907@gmail.com>
2019-11-20 16:30:17 +0900

    크론 추가

--- app/Http/Controllers/Room/LobbyController.php
+++ app/Http/Controllers/Room/LobbyController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Room;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\User;
 use App\Room;
 use App\Models\VideoInfo;
@@ -12,25 +11,36 @@ use Illuminate\Support\Facades\Auth;
 class LobbyController extends Controller
 {
     public function show(){
+        if(Auth::user()) {
+            $this->lobbyChecking();
+        }
+
+        $room_info = $this->roomInformation();
+
+        return view('lobby', ['room_info'=>$room_info[0], 'admission'=>$room_info[1]]);
+    }
+
+    static public function roomInformation(){
 
         $tb_video_info = (new Videoinfo())->getTable();
         $tb_room = (new Room())->getTable();
         $admission = Null;
 
-        $this->lobbyChecking();
+        $room_info[0] = Room::leftJoin($tb_video_info, function ($join) use ($tb_video_info, $tb_room) {
+            $join->on($tb_room.'.current_videoId', '=', $tb_video_info.'.videoId');
+        })->orderBy($tb_room.'.id', 'asc')->select([$tb_room.'.id', $tb_room.'.title', $tb_video_info.'.videoTitle'])->paginate(6);
 
-        $room_info = Room::leftJoin($tb_video_info, function ($join) use ($tb_video_info, $tb_room) {
-                $join->on($tb_room.'.current_videoId', '=', $tb_video_info.'.videoId');
-            })->orderBy($tb_room.'.id', 'asc')->get([$tb_room.'.id', $tb_room.'.title', $tb_video_info.'.videoTitle']);
-
-        for($i=0;$i<sizeof($room_info);$i++) {
-            $admission[$i] = User::where('room_id', $room_info[$i]->id)->count();
+        for($i=0;$i<sizeof($room_info[0]);$i++) {
+            $admission[$i] = User::where('room_id', $room_info[0][$i]->id)->count();
         }
 
-        return view('lobby', ['room_info'=>$room_info, 'admission'=>$admission]);
+        $room_info[1] = $admission;
+
+        return $room_info;
     }
 
-    public function lobbyChecking(){
-        User::where('nickname', Auth::user()->nickname)->update(['room_id'=>Null]);
+    public function lobbyChecking()
+    {
+        User::where('nickname', Auth::user()->nickname)->update(['room_id' => Null]);
     }
 }
