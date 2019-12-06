84b098e5308366ea8acf7b3eadff7ea2a0c732ab
bic4907 <bic4907@gmail.com>
2019-12-06 17:54:19 +0900

    방 상태 체크 If문을 Switch문으로 변경

--- app/Console/Commands/RoomManageDaemon.php
+++ app/Console/Commands/RoomManageDaemon.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Enums\RoomState;
 use App\Room;
 use Illuminate\Console\Command;
 
@@ -20,33 +21,41 @@ class RoomManageDaemon extends Command
         $rooms = Room::all();
         foreach ($rooms as $room) {
 
-            // 만약 방에 비디오가 틀어져 있지 않은 경우
-            if ($room->getRoomState() == RoomState::InActive) {
+            switch ($room->getRoomState()) {
 
-                $nextVideo = $room->getNextQueuedVideo();
-
-                // 만약 재생할 비디오가 있을 경우
-                if ($nextVideo != null) {
-                    $room->playVideo($nextVideo);
-                }
-
-            } else if($room->getRoomState() == RoomState::Active) {
-
-                if($room->isVideoGettingFinished() or $room->isRoomAlive() == false) {
+                // 방이 처음 만들어졌을 경우
+                case RoomState::InActive:
 
                     $nextVideo = $room->getNextQueuedVideo();
 
-                    if($nextVideo != null) {
-                        // 바로 다음 동영상 재생
+                    // 만약 재생할 비디오가 있을 경우
+                    if ($nextVideo != null) {
                         $room->playVideo($nextVideo);
-                    } else {
-                        // 만약 다음 비디오가 없어 다음 비디오 추가가 필요한 경우
-                        $rcVideo = $room->getRecommendedVideo($room->current_videoId);
-                        $room->enqueueVideo($rcVideo->videoId, $room->getCurrentHost());
                     }
 
-                }
+                    break;
+
+                // 방이 이미 사용중일 경우
+                case RoomState::Active:
+
+                    if ($room->isVideoGettingFinished() or $room->isRoomAlive() == false) {
+
+                        $nextVideo = $room->getNextQueuedVideo();
+
+                        if ($nextVideo != null) {
+                            // 바로 다음 동영상 재생
+                            $room->playVideo($nextVideo);
+                        } else {
+                            // 만약 다음 비디오가 없어 다음 비디오 추가가 필요한 경우
+                            $rcVideo = $room->getRecommendedVideo($room->current_videoId);
+                            $room->enqueueVideo($rcVideo->videoId, $room->getCurrentHost());
+                        }
+                    }
+
+                    break;
+
             }
+
         }
     }
 }
