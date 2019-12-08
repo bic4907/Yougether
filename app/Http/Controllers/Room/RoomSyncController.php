20d916a5d776bbfe2111f7ad25b082a1fa735324
eogh234 <eogh234@naver.com>
2019-12-09 04:15:07 +0900

    video/SyncController 이름 변경, 메소드 이름 변경/분리

--- app/Http/Controllers/Room/RoomSyncController.php
+++ app/Http/Controllers/Room/RoomSyncController.php
@@ -17,38 +17,39 @@ class RoomSyncController extends Controller
 
     public function updateRoomSync($room_id, Request $request) {
         // 2. 만약 요청한 유저가 호스트가 맞다면 방 정보를 갱신합니다.
-        $this->updateRoomSyncException($room_id, $request);
 
         $room = Room::findOrFail($room_id);
 
         $room->current_videoId = $request->input('videoId');
         $room->current_time = $request->input('videoTime');
         $room->current_videoStatus = VideoStatus::Playing;
+
+        $this->updateRoomSyncException($room); // 예외처리
+
         $room->save();
 
         // 3. 방 전체 유저들에게 BroadCasting 합니다 (방장제외)
         broadcast(new VideoSyncEvent($room_id, $room->current_videoId, VideoStatus::Playing, $room->current_time));
 
         return 'OK';
-    }
+    } //updateRoomSync()
 
-    public function updateRoomSyncException($room_id, Request $request)
+    public function updateRoomSyncException($room)
     {
-        if($request->input('videoId') == null or
-            $request->input('videoTime') == null
+        if($room->current_videoId == null or
+            $room->current_time == null
         ) abort(503);
 
         $user = Auth::user();
-        $room = Room::findOrFail($room_id);
 
         if(($user != null) and $room->current_host != $user->id) {
             abort(503);
         }
 
         //유저가 해당 방에서 비디오를 얼마나 되감기했는지 검사
-        if(!User::checkUserUpdateCount($user->id, $room_id))
+        if(!User::checkUserUpdateCount($user->id, $room->id))
         {
             abort(503);
         }
-    }
+    } //updateRoomSyncException()
 }
